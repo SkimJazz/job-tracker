4f172e41457fe45d265fe9d6f5b8932efa8e42a3
SkimJazz <jb.skimjazz@outlook.com>
2025-12-25 18:25:06 +1000

    feat: add FormRow component and update Register page to use reusable form inputs
    - Add reusable FormRow component with JSDoc and TODO for controlled input refactor
    - Refactor Register.jsx to use FormRow for all form fields
    - Add JSDoc file headers to Register.jsx and components/index.js
    - Update components/index.js to export FormRow

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,11 +1,52 @@
-import { Link } from "react-router-dom";
+/**
+ * @file Register.jsx
+ * @module Register
+ * @description
+ *   This module exports the Register React component, which renders a registration form for new users.
+ *   The form includes fields for user details (name, last name, location, email, password) and a submit button.
+ *   It also provides a link for existing members to navigate to the login page.
+ *   Styling is applied using the RegisterAndLoginPage wrapper and the shared Logo component.
+ */
 
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
+import { Link } from "react-router-dom";
+import { Logo, FormRow } from "../components";
+
+
+/**
+ * Register component renders a registration form for new users.
+ *
+ * The form includes fields for user name input and a submit button.
+ * It also provides a link for existing members to navigate to the login page.
+ *
+ * @component
+ * @returns {JSX.Element} The rendered registration form component.
+ */
 const Register = () => {
   return (
-    <div>
-      <h1>Register</h1>
-      <Link to="/login">Login</Link>
-    </div>
+    <Wrapper>
+      <form className="form">
+        <Logo />
+        <h4>register</h4>
+
+        <FormRow type="text" name="name" defaultValue="josh" />
+        <FormRow type="text" name="lastName" labelText="last name" defaultValue="skim" />
+        <FormRow type="text" name="location" defaultValue="cairns" />
+        <FormRow type="email" name="email" defaultValue="josh@email.com" />
+
+        <FormRow type="password" name="password" defaultValue="sec123"/>
+
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+        <p>
+          Already a member?
+          <Link to="/login" className="member-btn">
+            Login
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
   );
 };
 export default Register;
